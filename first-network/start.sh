d907116324a1890706933a686626ce6453b30d1c
Leonardo Porto <leonardoporto159ster@gmail.com>
2019-05-16 15:52:04 -0300

    Removido crypto-config do gitignore

--- first-network/start.sh
+++ first-network/start.sh
@@ -1,2 +1,2 @@
-IMAGE_TAG="latest" ../vendor/docker/docker-compose -f docker-compose-cli.yaml -f docker-compose-couch.yaml up -d 2>&1
-../vendor/docker/docker exec cli scripts/script.sh prontuchain 3 node 10 false
+IMAGE_TAG="latest" docker-compose -f docker-compose-cli.yaml -f docker-compose-couch.yaml up -d 2>&1
+docker exec cli scripts/script.sh prontuchain 3 node 10 false
